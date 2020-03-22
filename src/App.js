5e0a0bf85acea1dda4da019600acaa5b9a3434c6
Ronald Arias <ronald.arias@thoughtworks.com>
2020-03-22 18:26:50 -0500

    Added button on the main screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
-  return <h1>Breaking Bad</h1>;
+  const handleOnClick = () => {
+    console.log('consultando...');
+  };
+
+  return (
+    <Container>
+      <Button onClick={handleOnClick}>Obtener Frase</Button>
+    </Container>
+  );
 }
 
 export default App;
